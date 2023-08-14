c8e927e7622f4ee19b83439f14d2d7c6cf1e3e82
Michael Große <mic.grosse@googlemail.com>
2023-08-14 19:46:08 +0200

    Add RankSelector component

---
+++ src/components/RankSelector.js
@@ -0,0 +1,38 @@
+// This file is maintained at https://github.com/micgro42/mediawiki_userscripts
+const { CdxRadio, CdxField } = require('@wikimedia/codex');
+
+module.exports = {
+  name: 'RankSelector',
+  template: `<cdx-field :is-fieldset="true">
+		<cdx-radio
+			v-for="rank in ranks"
+			:style="{background: rank == 'preferred' ? '#E0FFE0' : rank == 'deprecated' ? '#FFE0E0' : ''}"
+			:key="'rank-' + rank"
+			:model-value="selected"
+			@update:model-value="$emit('update:selected', rank)"
+			name="rank-selector"
+			:input-value="rank"
+			:inline="true"
+		>
+			{{ rank }}
+		</cdx-radio>
+		<template #label>
+			Rank
+		</template>
+	</cdx-field>`,
+  components: {
+    CdxRadio,
+    CdxField,
+  },
+  props: {
+    selected: String,
+  },
+  emits: ['update:selected'],
+  setup() {
+    const ranks = ['normal', 'preferred', 'deprecated'];
+
+    return {
+      ranks,
+    };
+  },
+};
