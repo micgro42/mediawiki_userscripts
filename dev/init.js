a1da0d213ae977a487dfe7c454145aa5300befee
Michael Große <mic.grosse@googlemail.com>
2024-02-20 15:32:37 +0100

    init.js: Fix indentation
    At some point I should take the time and add a pre-commit hook for
    prettier.

--- dev/init.js
+++ dev/init.js
@@ -1,10 +1,10 @@
 switch (location.pathname) {
-case '/pointInTime':
-  window.loadApp(window.mockData.get('pointInTime'));
-  break;
-case '/quantityWithUnit':
-  window.loadApp(window.mockData.get('quantityWithUnit'));
-  break;
-case '/addStatement':
-  window.loadApp(null);
+  case '/pointInTime':
+    window.loadApp(window.mockData.get('pointInTime'));
+    break;
+  case '/quantityWithUnit':
+    window.loadApp(window.mockData.get('quantityWithUnit'));
+    break;
+  case '/addStatement':
+    window.loadApp(null);
 }
