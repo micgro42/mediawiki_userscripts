ade8212cf0cb1e850cc1c2938367405a4fa17b86
Michael Große <mic.grosse@googlemail.com>
2024-05-12 15:35:36 +0200

    Use a box-shadow for the rank color indicator instead of background
    This will work better with dark-mode.

--- src/components/RankSelector.js
+++ src/components/RankSelector.js
@@ -1,12 +1,13 @@
 // This file is maintained at https://github.com/micgro42/mediawiki_userscripts
 const { CdxRadio, CdxField } = require('@wikimedia/codex');
+const { computed } = require('vue');
 
 module.exports = {
   name: 'RankSelector',
   template: `<cdx-field :is-fieldset="true">
     <cdx-radio
       v-for="rank in ranks"
-      :style="{background: rank == 'preferred' ? '#E0FFE0' : rank == 'deprecated' ? '#FFE0E0' : ''}"
+      :style="rankStyle[rank]"
       :key="'rank-' + rank"
       :model-value="selected"
       @update:model-value="$emit('update:selected', rank)"
@@ -30,8 +31,20 @@ module.exports = {
   emits: ['update:selected'],
   setup() {
     const ranks = ['normal', 'preferred', 'deprecated'];
+    const rankStyle = computed(() => {
+      return {
+        normal: {},
+        preferred: {
+          boxShadow: '0px 0px 0px 5px #E0FFE0',
+        },
+        deprecated: {
+          boxShadow: '0px 0px 0px 5px #FFE0E0',
+        },
+      };
+    });
 
     return {
+      rankStyle,
       ranks,
     };
   },
